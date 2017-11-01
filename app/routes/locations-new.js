3d0cc71e4e08fd0147433c8fa623d84ae993c020
stephcmartin <martin.stephaniechow@gmail.com>
2017-11-01 13:39:56 -0400

    Created `ember generate component location-form` - Generate new component for a form to create a new locations.

--- app/routes/locations-new.js
+++ app/routes/locations-new.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+  createLocation(location){
+    let newLocation = this.get('store').createRecord('location', location);
+    newLocation.save().then(()=> this.transitionTo('locations'))
+  }
+}
 });
