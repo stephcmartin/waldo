07282879e5230e76b2cba5453774ec6d83955e42
stephcmartin <martin.stephaniechow@gmail.com>
2017-11-01 14:20:03 -0400

    Added Delete Location Feature
    - Add a delete button to the location component
    - Add an action for deleting a location
    - In route, persist delete to API with `.destroyRecord`
    
    Next steps: to only show 'delet button' when the user created the item.

--- app/routes/locations.js
+++ app/routes/locations.js
@@ -3,5 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model () {
     return this.get('store').findAll('location');
-}
+},
+actions: {
+  deleteLocation(location){
+    location.destroyRecord()
+  }
+},
 });
