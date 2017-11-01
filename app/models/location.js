82eff98499b1039453a70d5283771f52e9bb4fa8
stephcmartin <martin.stephaniechow@gmail.com>
2017-11-01 12:39:58 -0400

    Ember Generate Model Locations
    - create a model for my ember app to represent the
    location data from our API.

---
+++ app/models/location.js
@@ -0,0 +1,8 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  name: DS.attr('string'),
+  longitude: DS.attr('number'),
+  latitude: DS.attr('number'),
+  locationType: DS.attr('string')
+});
