a7d4049f2b722a23384bd647293b5a7263622f5f
stephcmartin <martin.stephaniechow@gmail.com>
2017-11-03 16:25:10 -0400

    Attempting to create goolge geocoding
    - added map to hbs.
    - Code in JS does not work yet.

--- app/routes/google-search.js
+++ app/routes/google-search.js
@@ -1,4 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-});
+  gMap: Ember.inject.service(),
+
+  standardGeocode(address) {
+    this.get('gMap')
+      .geocode({address})
+      .then((geocodes) => {
+        // Get first geocode latitude
+        console.log(geocodes[0].geometry.location.lat());
+      })
+      .catch(console.error);
+  }
+  // ,
+  //
+  // reverseGeocode(lat, lng) {
+  //   this.get('gMap')
+  //     .geocode({lat, lng})
+  //     .then((geocodes) => {
+  //       // Get first geocode address
+  //       console.log(geocodes[0].formatted_address);
+  //     })
+  //     .catch(console.error);
+  // }
+})
