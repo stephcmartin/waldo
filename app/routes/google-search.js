44820ed52a898a64de0d9663837b3a3a62877b50
stephcmartin <martin.stephaniechow@gmail.com>
2017-11-07 09:54:47 -0500

    Edited Lat and Long of Boston

--- app/routes/google-search.js
+++ app/routes/google-search.js
@@ -3,8 +3,8 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   setupController: function(controller) {
   controller.setProperties({
-    lat: 42,
-    lng: -71,
+    lat: 42.3601,
+    lng: -71.0589,
     zoom: 8
   });
 }
